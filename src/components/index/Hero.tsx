48de5d2a5a1a51ac729bb3e2071ae64dad2e9ff9
bozzhik <bozzhik@ya.ru>
2024-04-02 01:22:05 +0300

    install `axios` & fetch `Github` data for `Hero` block

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -1,4 +1,9 @@
+'use client'
+
+import {useEffect, useState} from 'react'
 import Image from 'next/image'
+
+import {fetchGithubData} from '@/lib/githubApi'
 
 import AvatarImage from '../../assets/images/index/avatar.jpg'
 import GithubIcon from '../../assets/images/icons/github.svg'
@@ -9,20 +14,17 @@ const heroContent = {
   caption: 'I write code and make design.',
   list: {
     1: {
-      number: 25,
       text: 'Repositories',
       link: 'https://github.com/bozzhik',
       icon: GithubIcon,
     },
     2: {
-      number: 244,
-      text: 'Total Commits in 2024',
+      text: 'Commits in 2024',
       link: 'https://github.com/bozzhik',
       icon: GainIcon,
     },
     3: {
-      number: 1220,
-      text: 'Total Code Symbols',
+      text: 'Pull Requests in 2024',
       link: 'https://github.com/bozzhik',
       icon: GithubIcon,
     },
@@ -31,6 +33,26 @@ const heroContent = {
 }
 
 export default function Hero() {
+  const [githubData, setGithubData] = useState<any>(null)
+
+  useEffect(() => {
+    const githubUsername = process.env.NEXT_PUBLIC_GITHUB_USERNAME
+    const githubToken = process.env.GITHUB_TOKEN
+
+    if (!githubUsername || !githubToken) {
+      console.error('GitHub username or token not provided.')
+      return
+    }
+
+    fetchGithubData(githubUsername, githubToken)
+      .then((data) => {
+        setGithubData(data)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }, [])
+
   return (
     <div className="flex flex-col gap-8">
       <div>
@@ -40,12 +62,18 @@ export default function Hero() {
       <div className="flex flex-row items-center gap-6">
         <Image className="rounded-lg bg-custom-b-gray" width="85" height="85" src={AvatarImage} priority quality={100} alt="avatar" />
         <ul className="space-y-2">
-          {Object.values(heroContent.list).map((item) => (
-            <li key={item.text}>
+          {Object.values(heroContent.list).map((item, index) => (
+            <li key={index}>
               <a className="flex items-center gap-3" href={item.link}>
                 <Image className="s-5" src={item.icon} alt={item.text} />
                 <div className="space-x-1">
-                  <span>{item.number}</span>
+                  {githubData && (
+                    <span>
+                      {index === 0 && githubData.repositories.length}
+                      {index === 1 && githubData.totalCommits}
+                      {index === 2 && githubData.totalPullRequests}
+                    </span>
+                  )}
                   <span>{item.text}</span>
                 </div>
               </a>
