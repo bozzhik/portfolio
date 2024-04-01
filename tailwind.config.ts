740fc17d44020d4b1a7e3183d01d7902e96e0f71
bozzhik <bozzhik@ya.ru>
2024-04-01 21:17:33 +0300

    set up project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import {Config} from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  mode: 'jit',
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        custom: {
+          violet: '#911BED',
+        },
       },
     },
+    screens: {
+      xl: {max: '1536px'},
+      lg: {max: '1024px'},
+      sm: {max: '768px'},
+      xs: {max: '350px'},
+    },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [
+    plugin(function sizePlugin(api) {
+      api.matchUtilities({s: (value: string) => ({width: value, height: value})}, {values: api.theme('width')})
+    }),
+  ],
+}
+
+export default config
