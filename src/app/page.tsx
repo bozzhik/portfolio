5c6b325f39f008fa66f3a8b1ced6ae454122db63
bozzhik <bozzhik@ya.ru>
2024-04-19 01:43:30 +0300

    `---HARD RESET---`
    let's redesign this again 🤬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Hero from '@/components/index/Hero'
-
-export default function Home() {
-  return (
-    <section data-section="index" className="flex flex-col gap-24 sm:gap-16">
-      <Hero />
-    </section>
-  )
+export default function Index() {
+  return <section>hey!</section>
 }
