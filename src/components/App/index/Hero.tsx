5579b54ee9cd812095afbe50ff975e555b8dd111
bozzhik <bozzhik@ya.ru>
2024-04-19 20:53:27 +0300

    use `Badge` inside of `Hero` index block

--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import CursorImage from '%/icons/cursor.svg'
 
 import {Text} from '#/UI/Text'
+import {Badge} from '#/UI/Badge'
 
 import '%%/stylesheets/animate-surfing.css'
 
@@ -9,13 +10,14 @@ export default function Hero() {
   return (
     <section data-section="hero-index" className="space-y-8 ">
       <div className="group">
-        <Text type="heading">
-          hey I&#39;m bozzhik
+        <Text type="heading" className="flex items-center">
+          hey, I&#39;m bozzhik
           <Image className="w-5 ml-4 animate-surfing group-hover:scale-[115%] duration-300" src={CursorImage} alt="" />
         </Text>
       </div>
-
-      <Text>I&#39;m a website developer and UX/UI designer. I am studying at HSE to design and develop digital products. I am currently working as a self-employed frontend developer, I create beautiful landing pages and interactive multi-page websites.</Text>
+      <Text>
+        I&#39;m a website developer and user interface designer. I am studying at the HSE to design and develop <Badge href="#" icon={CursorImage} text="digital products" />. <br className="hidden sm:block" /> I am currently working as a self-employed frontend developer, specializing in creating stunning <Badge href="#" icon={CursorImage} text="landing pages" /> and interactive <Badge href="#" icon={CursorImage} text="websites" />.
+      </Text>
     </section>
   )
 }
--- src/components/UI/Text.tsx
+++ src/components/UI/Text.tsx
@@ -2,15 +2,15 @@ import {cn} from '@/lib/utils'
 
 interface Props {
   type?: 'heading' | 'text'
-  children: React.ReactNode
   className?: string
+  children: React.ReactNode
 }
 
 const Text: React.FC<Props> = ({type = 'text', children, className}) => {
   if (type === 'heading') {
-    return <h1 className={cn('flex items-center text-2xl font-medium tracking-tight', className)}>{children}</h1>
+    return <h1 className={cn('text-2xl font-medium tracking-tight', className)}>{children}</h1>
   } else if (type === 'text') {
-    return <p className={cn('flex items-center text-custom-gray-d4 leading-7', className)}>{children}</p>
+    return <p className={cn('text-custom-gray-d4 leading-[1.8rem]', className)}>{children}</p>
   }
 }
 
