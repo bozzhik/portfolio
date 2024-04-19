b816458afb395f44ae24c078f5be299499dfd185
bozzhik <bozzhik@ya.ru>
2024-04-19 20:03:56 +0300

    add `animate-surfing` for cursor in `Hero` index block

--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -3,12 +3,18 @@ import CursorImage from '%/icons/cursor.svg'
 
 import {Text} from '#/UI/Text'
 
+import '%%/stylesheets/animate-surfing.css'
+
 export default function Hero() {
   return (
-    <section data-section="hero-index" className="space-y-8">
-      <Text type="heading">
-        hey I&#39;m bozzhik <Image className="w-5 ml-4" src={CursorImage} alt="" />
-      </Text>
+    <section data-section="hero-index" className="space-y-8 ">
+      <div className="group">
+        <Text type="heading">
+          hey I&#39;m bozzhik
+          <Image className="w-5 ml-4 animate-surfing group-hover:scale-[115%] duration-300" src={CursorImage} alt="" />
+        </Text>
+      </div>
+
       <Text>I&#39;m a website developer and UX/UI designer. I am studying at HSE to design and develop digital products. I am currently working as a self-employed frontend developer, I create beautiful landing pages and interactive multi-page websites.</Text>
     </section>
   )
