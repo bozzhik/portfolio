99e6cdc203c8e7db8455f449b366d082fe67671f
bozzhik <bozzhik@ya.ru>
2024-04-20 18:00:57 +0300

    remake `Badge` UI component

--- src/components/UI/Badge.tsx
+++ src/components/UI/Badge.tsx
@@ -1,23 +1,29 @@
 import {cn} from '@/lib/utils'
-
 import Link from 'next/link'
 import Image from 'next/image'
 
 interface Props {
   text: string
-  href: string
+  href?: string
   icon?: string
   className?: string
 }
 
 const Badge: React.FC<Props> = ({icon, href, text, className}) => {
-  return (
-    <Link href={href} className={cn('group inline-flex items-center rounded border p-1 text-sm leading-4 no-underline border-neutral-700 bg-custom-gray text-neutral-100', className)}>
-      {icon && <Image className="object-contain mr-1 s-3.5" src={icon} alt={`icon ${text}`} />}
-
+  const badgeStyles = cn('group w-fit inline-flex items-center rounded border p-1 text-sm leading-4 no-underline border-neutral-700 bg-custom-gray text-neutral-100', className)
+  const BadgeContent = (
+    <>
+      {icon && <Image className="object-contain mr-1 s-3.5 group-hover:rotate-[-15deg] duration-300" src={icon} alt={`icon ${text}`} />}
       <span className="text-white" dangerouslySetInnerHTML={{__html: text || ''}} />
+    </>
+  )
+
+  return href ? (
+    <Link href={href} className={badgeStyles}>
+      {BadgeContent}
     </Link>
+  ) : (
+    <mark className={badgeStyles}>{BadgeContent}</mark>
   )
 }
-
 export {Badge}
