61bc485594caea9a31ec847bee7b5ed6f41966e0
bozzhik <bozzhik@ya.ru>
2024-04-20 03:53:34 +0300

    integrate `Sanity` in NEXT project

---
+++ src/lib/sanity.ts
@@ -0,0 +1,15 @@
+import {createClient} from 'next-sanity'
+import ImageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+  projectId: 'my0gy5hm',
+  dataset: 'production',
+  apiVersion: '2024-01-08',
+  useCdn: false,
+})
+
+const imageBuilder = ImageUrlBuilder(client)
+
+export function urlForImage(source: any) {
+  return imageBuilder.image(source)
+}
