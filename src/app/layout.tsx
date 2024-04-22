e17336f24d5bdcdda280511f2789f5fa0971a89d
bozzhik <bozzhik@ya.ru>
2024-04-22 04:22:38 +0300

    remove `unnecessary` fonts from project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,28 +9,12 @@ import Particles from '#/Global/Particles'
 const SuisseIntl = localFont({
   src: [
     {
-      path: '../assets/fonts/SuisseIntl-Light.woff2',
-      weight: '300',
-    },
-    {
       path: '../assets/fonts/SuisseIntl-Regular.woff2',
       weight: '400',
     },
     {
-      path: '../assets/fonts/SuisseIntl-Book.woff2',
-      weight: '450',
-    },
-    {
       path: '../assets/fonts/SuisseIntl-Medium.woff2',
       weight: '500',
-    },
-    {
-      path: '../assets/fonts/SuisseIntl-SemiBold.woff2',
-      weight: '600',
-    },
-    {
-      path: '../assets/fonts/SuisseIntl-Bold.woff2',
-      weight: '700',
     },
   ],
 })
