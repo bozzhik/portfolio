ae3e614de8a66f75eb05509a7b536ef2fc122f65
maxim <bozzhik@ya.ru>
2024-07-26 01:52:07 +0300

    refactor `Hero` index block

--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -3,31 +3,38 @@ import CursorImage from '%/icons/cursor.svg'
 
 import Text from '#/UI/Text'
 import Badge from '#/UI/Badge'
-import {ArrowLink} from '#/UI/ArrowLink'
+import ArrowLink from '#/UI/ArrowLink'
 
 import '%%/stylesheets/animate-surfing.css'
 
-export default function Hero() {
-  const links = [
-    {text: 'E-mail', url: 'mailto:bozzhik@yandex.com'},
-    {text: 'GitHub', url: 'https://github.com/bozzhik'},
-    {text: 'Telegram', url: 'https://t.me/bozzhik'},
-    {text: 'Instagram', url: 'https://instagram.com/bozzzzhik'},
-  ]
+const links = [
+  {text: 'E-mail', url: 'mailto:bozzhik@yandex.com'},
+  {text: 'GitHub', url: 'https://github.com/bozzhik'},
+  {text: 'Telegram', url: 'https://t.me/bozzhik'},
+  {text: 'Instagram', url: 'https://instagram.com/bozzzzhik'},
+]
 
+export default function Hero() {
   return (
-    <section data-section="hero-index" className="space-y-8">
-      <div className="group">
-        <Text type="heading" className="flex items-center">
-          hey, I&#39;m bozzhik
-          <Image className="w-5 ml-4 animate-surfing group-hover:scale-[115%] duration-300" src={CursorImage} alt="" />
+    <section data-section="hero-index" className="space-y-7">
+      <div className="space-y-5">
+        <div className="group w-fit">
+          <Text type="heading" className="flex items-center">
+            hey, I&#39;m bozzhik
+            <Image className="w-5 ml-4 animate-surfing group-hover:scale-[115%] duration-300" src={CursorImage} alt="" />
+          </Text>
+        </div>
+
+        <Text>
+          I&#39;m a website developer and user interface designer. I am studying at HSE to design and develop
+          <Badge href="#PROJECTS" icon={CursorImage} text="digital products" />.
+          <br className="hidden sm:block" />
+          <div className="hidden sm:block sm:s-3" /> Currently, I work as a self-employed frontend developer, specializing in creating impressive
+          <Badge href="/works" className="mr-[0.5px]" icon={CursorImage} text="websites" /> that boost business.
         </Text>
       </div>
-      <Text>
-        I&#39;m a website developer and user interface designer. I am studying at the HSE to design and develop <Badge href="#PROJECTS" icon={CursorImage} text="digital products" />. <br className="hidden sm:block" /> I am currently working as a self-employed frontend developer, specializing in creating stunning <Badge href="/works/" icon={CursorImage} text="landing pages" /> and interactive <Badge href="/works/" className="mr-[0.5px]" icon={CursorImage} text="websites" />.
-      </Text>
 
-      <div className="flex gap-5 xl:gap-3.5 sm:flex-wrap sm:gap-5 text-neutral-300">
+      <div className="flex gap-5 xl:gap-3.5 sm:flex-wrap sm:gap-x-3.5 sm:gap-y-2 text-neutral-300">
         {links.map((link, index) => (
           <ArrowLink key={index} href={link.url} text={link.text} />
         ))}
