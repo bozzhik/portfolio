011f1e5f5077b697170f42b77a516e1ebf01ddd2
maxim <bozzhik@ya.ru>
2024-07-25 20:50:17 +0300

    enable `html` in `text` type `Text` UI component

--- src/components/UI/Text.tsx
+++ src/components/UI/Text.tsx
@@ -3,13 +3,19 @@ import {cn} from '@/lib/utils'
 interface Props {
   type?: 'heading' | 'text'
   className?: string
-  children: React.ReactNode
+  children: React.ReactNode | string // Allow string for HTML content
 }
 
 const Text: React.FC<Props> = ({type = 'text', children, className}) => {
   if (type === 'heading') {
     return <h1 className={cn('text-2xl font-medium tracking-tight', className)}>{children}</h1>
   } else if (type === 'text') {
+    const hasHTML = typeof children === 'string' && /<\/?[a-z][\s\S]*>/i.test(children)
+
+    if (hasHTML) {
+      return <p className={cn('text-neutral-300 leading-[1.8rem]', className)} dangerouslySetInnerHTML={{__html: children}} />
+    }
+
     return <p className={cn('text-neutral-300 leading-[1.8rem]', className)}>{children}</p>
   }
 }
