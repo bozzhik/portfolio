e8ec791bc554c1095f2aaaf07a50c3d0305062c2
maxim <bozzhik@ya.ru>
2024-07-26 01:11:21 +0300

    init merged type `Product` in `product` types

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,4 +1,4 @@
-export interface Work {
+export type Product = {
   name: string
   link: string
   id: number
@@ -8,16 +8,4 @@ export interface Work {
   hover_color?: any
   is_best?: boolean
   in_development?: boolean
-}
-
-export interface Project {
-  name: string
-  link: string
-  id: number
-  description: string
-  type: string
-  image: Array<{asset: {url: string}}>
-  hover_color?: any
-  is_best: boolean
-  in_development: boolean
 }
