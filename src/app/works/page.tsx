e91d13bb34f5b001a28f3981c50366de10aeb3b7
maxim <bozzhik@ya.ru>
2024-07-26 03:14:02 +0300

    remake `/works` page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,10 +1,31 @@
-import Container from '#/Global/Container'
-import Works from '##/index/Works'
+import {Product} from '@/types/product'
+import {getWorks} from '##/index/Works'
 
-export default function WorksPage() {
+import Text from '#/UI/Text'
+import ProductCard from '#/UI/ProductCard'
+
+import Container from '#/Global/Container'
+
+export default async function WorksPage() {
+  const works: Product[] = await getWorks()
+
+  if (!works) {
+    return <mark>Произошла ошибка при получении данных!</mark>
+  }
+
+  works.sort((a, b) => a.id - b.id)
+
   return (
-    <Container className="space-y-14">
-      <Works />
+    <Container className="space-y-6">
+      <div className="space-y-4 sm:space-y-3">
+        <Text type="heading">my works</Text>
+      </div>
+
+      <div className="flex flex-col gap-5 sm:gap-4">
+        {works.map((work, index) => (
+          <ProductCard type="work" product={work} key={index} />
+        ))}
+      </div>
     </Container>
   )
 }
