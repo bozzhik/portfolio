2be1dac6f61de6f82a099b220ce9fa6ec42d7bd1
maxim <bozzhik@ya.ru>
2024-07-26 03:27:45 +0300

    set up `Header` Global component for `/cv` page

--- src/components/UI/ArrowLink.tsx
+++ src/components/UI/ArrowLink.tsx
@@ -5,13 +5,14 @@ interface Props {
   href: string
   text: string
   target?: boolean
+  active?: boolean
   className?: string
   svgClassName?: string
 }
 
-export default function ArrowLink({href, text, target = true, className, svgClassName}: Props) {
+export default function ArrowLink({href, text, target = true, active = false, className, svgClassName}: Props) {
   const ArrowIcon = (
-    <svg className={cn('duration-500 fill-neutral-300 s-5 group-hover:rotate-45 group-hover:fill-neutral-400', svgClassName)} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <svg className={cn('duration-500 fill-neutral-300 s-5 group-hover:rotate-45 group-hover:fill-neutral-400', active ? 'rotate-45 fill-neutral-400' : '', svgClassName)} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path d="M6 7v2h8.59L5 17.59 6.41 19 16 9.41V18h2V6z"></path>
     </svg>
   )
@@ -19,7 +20,7 @@ export default function ArrowLink({href, text, target = true, className, svgClas
   return (
     <Link href={href} className={cn('flex items-center gap-1 group', className)} {...(target && {target: '_blank'})}>
       {ArrowIcon}
-      <span className="duration-500 group-hover:text-neutral-400">{text}</span>
+      <span className={cn('duration-500 group-hover:text-neutral-400', active ? 'text-neutral-400' : '')}>{text}</span>
     </Link>
   )
 }
