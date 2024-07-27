d28d03ff7d26b3e703b90ac0ca71f2ff4729ceed
maxim <bozzhik@ya.ru>
2024-07-28 01:09:21 +0300

    optimize `Hero` cv block (component)

--- src/components/App/cv/Hero.tsx
+++ src/components/App/cv/Hero.tsx
@@ -7,19 +7,17 @@ import '%%/stylesheets/animate-surfing.css'
 
 export default function Hero() {
   return (
-    <section data-section="hero-cv" className="space-y-7">
-      <div className="space-y-5">
+    <section data-section="hero-cv">
+      <div className="space-y-3">
         <div className="group w-fit">
           <Text type="heading" className="flex items-center">
-            hey, I&#39;m bozzhik
+            Maxim Bozhik
             <Image className="w-5 ml-4 animate-surfing group-hover:scale-[115%] duration-300" src={CursorImage} alt="" />
           </Text>
         </div>
-
         <Text>
-          I&#39;m a website developer and user interface designer. I am studying at HSE to design and develop digital products.
-          <br className="hidden sm:block" />
-          <span className="hidden sm:block sm:s-3" /> Currently, I work as a self-employed frontend developer, specializing in creating impressive websites that boost business.
+          I&#39;m a website developer and user interface designer. Currently, I work as a self-employed frontend developer, specializing in creating impressive websites that boost business.
+          <span className="hidden sm:block sm:s-3" />
         </Text>
       </div>
     </section>
