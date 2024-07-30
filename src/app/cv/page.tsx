f645742437fe15d80955347bc34207d5fc8eb1d2
maxim <bozzhik@ya.ru>
2024-07-30 15:29:21 +0300

    init `Block` cv sub-block

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,12 +1,23 @@
 import Container from '#/Global/Container'
+
 import Hero from '##/cv/Hero'
 import ImageGrid from '##/cv/ImageGrid'
+import Connect from '##/cv/Connect'
+
+import Block from '##/cv/Block'
+import Text from '#/UI/Text'
 
 export default function CvPage() {
   return (
-    <Container className="space-y-8 sm:space-y-6">
+    <Container className="space-y-8">
       <Hero />
       <ImageGrid />
+
+      <Block index="about" heading="about me">
+        <Text>I am a student at HSE University, pursuing a Bachelor&#39;s degree in Computer Science. With a strong passion for technology, software, and design, I am always eager to expand my knowledge and skills in these fields. Additionally, my interests in 3D modeling and product promotion beautifully complement my technical studies.</Text>
+      </Block>
+
+      <Connect />
     </Container>
   )
 }
