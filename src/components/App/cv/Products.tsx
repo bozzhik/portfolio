e85b8374c0851feef2dd32fb4609fff5aa579bc0
maxim <bozzhik@ya.ru>
2024-07-31 01:57:11 +0300

    mix `projects` with `works` for `Products` cv block

--- src/components/App/cv/Products.tsx
+++ src/components/App/cv/Products.tsx
@@ -1,24 +1,30 @@
 import {Product} from '@/types/product'
 import {getWorks} from '##/index/Works'
-import ProductCard from '#/UI/ProductCard'
+import {getProjects} from '##/index/Projects'
+
+import Link from 'next/link'
 
 import Block from '##/cv/Block'
 import Text from '#/UI/Text'
+import ProductCard from '#/UI/ProductCard'
 
 export default async function Products() {
   const works: Product[] = await getWorks()
+  const projects: Product[] = await getProjects()
 
-  const worksList = {
-    1: 'graphite-house',
+  const productList = {
+    1: 'murino-arena',
     2: 'udelnaya',
   }
 
-  if (!works) {
+  if (!works || !projects) {
     return <mark>Произошла ошибка при получении данных!</mark>
   }
 
-  const slugsInOrder = Object.values(worksList)
-  const filteredAndSortedWorks = works.filter((work) => slugsInOrder.includes(work.slug)).sort((a, b) => slugsInOrder.indexOf(a.slug) - slugsInOrder.indexOf(b.slug))
+  const allProducts = [...works, ...projects]
+  const slugsInOrder = Object.values(productList)
+
+  const filteredAndSortedProducts = allProducts.filter((product) => slugsInOrder.includes(product.slug)).sort((a, b) => slugsInOrder.indexOf(a.slug) - slugsInOrder.indexOf(b.slug))
 
   return (
     <Block index="projects" heading="projects">
@@ -27,10 +33,14 @@ export default async function Products() {
       </Text>
 
       <div className="flex flex-col gap-5 sm:gap-4">
-        {filteredAndSortedWorks.map((work, index) => (
-          <ProductCard type="work" product={work} key={index} />
+        {filteredAndSortedProducts.map((product, index) => (
+          <ProductCard type="work" product={product} key={index} />
         ))}
       </div>
+
+      <Link href="/works" className="w-full block py-2 font-book sm:font-normal text-center hover:text-white/60 duration-200 sm:text-base from-neutral-900/50 to-neutral-900/50 bg-gradient-to-b rounded-xl sm:rounded-lg border-[1px] border-neutral-800 border-b-0">
+        View all
+      </Link>
     </Block>
   )
 }
