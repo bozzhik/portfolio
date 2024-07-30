08a383beb18a8fec269cecef4576fda111b6062f
maxim <bozzhik@ya.ru>
2024-07-30 17:02:26 +0300

    add `hover` interactivity for `Jobs` cv block

--- src/components/App/cv/Jobs.tsx
+++ src/components/App/cv/Jobs.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import FreelanceImage from '%/cv/freelance.jpg'
 import StarNetImage from '%/cv/starnet.jpg'
 
+import {ArrowUpRight} from 'lucide-react'
+
 import Block from '##/cv/Block'
 import Text from '#/UI/Text'
 
@@ -10,14 +12,14 @@ const jobsData: any = {
     title: 'Freelance',
     caption: 'Developer and UX/UI designer',
     duration: 'Nov 2021 - Now',
-    link: 'starnet.md',
+    link: '/works',
     logo: FreelanceImage,
   },
   'star-net': {
     title: 'StarNet Solutions',
     caption: 'Junior Frontend developer',
     duration: 'May 2020 - Sep 2021',
-    link: 'starnet.md',
+    link: 'https://www.starnet.md',
     logo: StarNetImage,
   },
 }
@@ -34,19 +36,21 @@ export default function Jobs() {
           {Object.keys(jobsData).map((key) => {
             const {title, caption, duration, link, logo} = jobsData[key]
             return (
-              <a href={link} key={key} className="flex items-center justify-between sm:items-start sm:flex-col">
+              <a href={link} className="flex items-center justify-between group sm:items-start sm:flex-col" target="_blank" key={key}>
                 <div className="flex items-center justify-between gap-4">
                   <Image src={logo} alt={`${title} logo`} width={100} height={100} className="rounded-md s-14 sm:s-16" />
 
                   <div>
                     <Text className="text-lg font-medium text-white sm:text-base">{title}</Text>
                     <Text className="leading-normal text-neutral-400">{caption}</Text>
-
-                    <Text className="hidden text-neutral-400 sm:block">{duration}</Text>
+                    <Text className="hidden text-neutral-400 group-hover:text-white sm:block">{duration}</Text>
                   </div>
                 </div>
 
-                <Text className="duration-200 text-neutral-400 sm:hidden">{duration}</Text>
+                <div className="flex items-center sm:hidden">
+                  <Text className="px-1 duration-300 bg-custom-black translate-x-7 text-neutral-400 group-hover:text-white group-hover:-translate-x-2">{duration}</Text>
+                  <ArrowUpRight className="-mt-0.5 duration-500 opacity-0 group-hover:opacity-100 -z-10" />
+                </div>
               </a>
             )
           })}
