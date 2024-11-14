db25e0f3bcaf28e715e03b9862a929d165f422c8
maxim <bozzhik@ya.ru>
2024-11-14 23:25:18 +0300

    update `Jobs` cv block

--- src/components/App/cv/Jobs.tsx
+++ src/components/App/cv/Jobs.tsx
@@ -1,34 +1,34 @@
 import Block from '#/UI/Block'
 import ItemLink from '#/UI/ItemLink'
 
-import FreelanceImage from '%/cv/freelance.jpg'
-import MarakuImage from '%/cv/maraku.jpg'
-import StarNetImage from '%/cv/starnet.jpg'
+import PartisansLogo from '%/cv/partisans.jpg'
+import MarakuLogo from '%/cv/maraku.jpg'
+import StarNetLogo from '%/cv/starnet.jpg'
 
 const jobsData: any = {
   heading: 'work',
   text: 'Below is a list of companies where I have worked as a <span class="text-white">Software Developer</span>',
   items: {
     'custom-freelance': {
-      title: 'Freelance',
-      caption: 'Developer and UX/UI designer',
-      duration: 'Nov 2021 - Present',
-      link: '/works',
-      icon: FreelanceImage,
+      title: 'Partisans',
+      caption: 'Frontend Developer',
+      duration: 'Aug 2024 - Present',
+      link: 'https://www.partisans.agency/',
+      icon: PartisansLogo,
     },
     'maraku-ru': {
       title: 'Maraku',
       caption: 'Frontend Developer and Designer',
       duration: 'Apr 2022 - May 2023',
       link: 'https://www.maraku.ru',
-      icon: MarakuImage,
+      icon: MarakuLogo,
     },
     'star-net': {
       title: 'StarNet Solutions',
       caption: 'Junior Frontend developer',
       duration: 'May 2020 - Sep 2021',
       link: 'https://www.starnet.md',
-      icon: StarNetImage,
+      icon: StarNetLogo,
     },
   },
 }
