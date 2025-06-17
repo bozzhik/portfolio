64443981a202954385e06055d0c962fb8f67c54e
maxim <bozzhik@ya.ru>
2025-06-17 03:58:00 +0300

    init `content` storage file for website

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,39 +1,27 @@
-import BozzhikImage from '$/bozzhik.jpg'
-import GithubIcon from '$/icons/github.svg'
-import EmailIcon from '$/icons/email.svg'
-
+import {CONTENT} from '@/content'
 import {cn} from '@/lib/utils'
 
 import Link from 'next/link'
 import Image from 'next/image'
 
-const socials = {
-  github: {
-    link: 'https://github.com/bozzhik',
-    icon: GithubIcon,
-  },
-  email: {
-    link: 'mailto:bozzhik@yandex.com',
-    icon: EmailIcon,
-  },
-}
-
 export default function Header() {
+  const {person, socials} = CONTENT.global
+
   return (
-    <header className={cn('max-w-2xl xl:max-w-[35rem] mx-auto sm:mx-4', 'mt-20', 'flex justify-between')}>
+    <header className={cn('max-w-2xl xl:max-w-[35rem] mx-auto sm:mx-4', 'mt-20', 'flex items-center justify-between')}>
       <div className="flex items-center gap-3.5">
-        <Image quality={100} className="size-14 rounded-[10px]" src={BozzhikImage} alt={`Maxim Bozhik — Frontend & Mobile Developer`} />
+        <Image quality={100} className="size-14 rounded-[10px]" src={person.picture} alt={`${person.name} — Frontend & Mobile Developer`} />
 
         <div>
-          <h1 className="text-xl font-semibold text-foreground">Maxim Bozhik</h1>
-          <p className="text-base text-gray">Turning caffeine into code.</p>
+          <h1 className="text-xl font-semibold text-foreground">{person.name}</h1>
+          <p className="text-base text-gray">{person.taglines[Math.floor(Math.random() * person.taglines.length)]}</p>
         </div>
       </div>
 
       <div className="flex gap-1.5">
         {Object.entries(socials).map(([key, item]) => (
-          <Link className={cn('p-1.75', 'grid place-items-center size-fit', 'bg-gray-dark rounded-[10px] border border-foreground/10')} href={item.link} target="_blank" rel="noopener noreferrer" key={key}>
-            <Image className="size-6" src={item.icon} alt={`${key} icon`} />
+          <Link className={cn('p-2', 'grid place-items-center size-fit', 'bg-gray-dark rounded-[10px] border border-foreground/10')} href={item.link} target="_blank" rel="noopener noreferrer" key={key}>
+            <Image className="size-5.5" src={item.icon} alt={`${key} icon`} />
           </Link>
         ))}
       </div>
---
+++ src/content.ts
@@ -0,0 +1,23 @@
+import BozzhikImage from '$/bozzhik.jpg'
+import GithubIcon from '$/icons/github.svg'
+import EmailIcon from '$/icons/email.svg'
+
+export const CONTENT = {
+  global: {
+    person: {
+      name: 'Maxim Bozhik',
+      taglines: ['Turning caffeine into code.', 'Making pixels push themselves.', 'Just a dev doing dev things.', 'Coding, creating, conquering (bugs).', `Making websites work, so you don't have to.`],
+      picture: BozzhikImage,
+    },
+    socials: {
+      github: {
+        link: 'https://github.com/bozzhik',
+        icon: GithubIcon,
+      },
+      email: {
+        link: 'mailto:bozzhik@yandex.com',
+        icon: EmailIcon,
+      },
+    },
+  },
+}
